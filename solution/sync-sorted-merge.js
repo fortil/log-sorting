18ffb13e2b21cb8ef8f4b4d6be404457c1895a6b
William Penagos <billalpeza@gmail.com>
2021-03-05 19:50:10 -0500

    feat(sync solution was done): was done the sync solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,18 @@
 "use strict";
-
+const _ = require("lodash");
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const entries = logSources.map(i => i.pop()).sort((object1, object2) => {
+    const t1 = new Date(object1.date).getTime();
+    const t2 = new Date(object2.date).getTime();
+    if (t1 === t2) {
+      return 0;
+    }
+    return t1 > t2 ? 1 : -1;
+  }).forEach((obj) => {
+    printer.print(obj);
+  });
+  printer.done();
   return console.log("Sync sort complete.");
 };
